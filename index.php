f25d8eb6d3c97dab2d095ebd35d2dba5e121b4ec
000menez000 <menezeskauah@gmail.com>
2025-02-25 14:59:32 -0300

    corrigindo bugs

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-header('Access-Control-Allow-Origins: *'); // Libera para que qualquer domínio consiga acessar a API.
+header('Access-Control-Allow-Origin: *'); // Libera para que qualquer domínio consiga acessar a API.
 header('Content-type: application/json'); // Configura para que todos os dados retornem em JSON.
 
-date_default_timezone_get('America/Sao_Paulo'); // Configura a data e a hora do sistema para horário de São Paulo.
+date_default_timezone_set('America/Sao_Paulo'); // Configura a data e a hora do sistema para horário de São Paulo.
 
 if (isset($_GET['path'])) {
     $path = explode("/", $_GET['path']);
