f943d0809bfbbf432a23b5f002534deb4f7fe818
000menez000 <menezeskauah@gmail.com>
2025-02-25 14:51:56 -0300

    criando configurações iniciais da API

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "hello world\n";
-echo 1+1;
+header('Access-Control-Allow-Origins: *'); // Libera para que qualquer domínio consiga acessar a API.
+header('Content-type: application/json'); // Configura para que todos os dados retornem em JSON.
+
+date_default_timezone_get('America/Sao_Paulo'); // Configura a data e a hora do sistema para horário de São Paulo.
+
+if (isset($_GET['path'])) {
+    $path = explode("/", $_GET['path']);
+} else { echo "Caminho não existe"; exit; }
+
+if (isset($path[0])) {
+    $api = $path[0];
+} else { echo " "; }
+
+// var_dump($path);
