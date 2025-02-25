8f195ef58cbf4c0e5681e458abdf934be4fa1186
000menez000 <menezeskauah@gmail.com>
2025-02-25 15:48:52 -0300

    corrigindo bug do arquivo .htaccess

--- index.php
+++ index.php
@@ -4,12 +4,4 @@ header('Content-type: application/json'); // Configura para que todos os dados r
 
 date_default_timezone_set('America/Sao_Paulo'); // Configura a data e a hora do sistema para horário de São Paulo.
 
-if (isset($_GET['path'])) {
-    $path = explode("/", $_GET['path']);
-} else { echo "Caminho não existe"; exit; }
-
-if (isset($path[0])) {
-    $api = $path[0];
-} else { echo " "; }
-
-// var_dump($path);
+var_dump($_GET['path']);
